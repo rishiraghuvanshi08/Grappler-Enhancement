54e34721df4281595cbfcac4b83f5540b8569aa0
rishiraghuvanshi08 <rishiraghuvanshi08@gmail.com>
2023-10-16 13:23:11 +0530

    First Commit

---
+++ src/main/java/com/grapplermodule1/GrapplerEnhancement/GrapplerEnhancementApplication.java
@@ -0,0 +1,13 @@
+package com.grapplermodule1.GrapplerEnhancement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapplerEnhancementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapplerEnhancementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grapplermodule1/GrapplerEnhancement/GrapplerEnhancementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grapplermodule1.GrapplerEnhancement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapplerEnhancementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
