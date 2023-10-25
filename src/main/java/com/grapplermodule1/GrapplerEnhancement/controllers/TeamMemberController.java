a390935d06b9e49982a17cbf4b50dc9b57230a9d
rishiraghuvanshi08 <rishiraghuvanshi08@gmail.com>
2023-10-25 19:28:14 +0530

    Updated Controller

--- src/main/java/com/grapplermodule1/GrapplerEnhancement/controllers/TeamMemberController.java
+++ src/main/java/com/grapplermodule1/GrapplerEnhancement/controllers/TeamMemberController.java
@@ -1,34 +1,130 @@
 package com.grapplermodule1.GrapplerEnhancement.controllers;
 
+import com.grapplermodule1.GrapplerEnhancement.customexception.*;
+import com.grapplermodule1.GrapplerEnhancement.dtos.TeamMembersDTO;
+import com.grapplermodule1.GrapplerEnhancement.dtos.UsersDTO;
 import com.grapplermodule1.GrapplerEnhancement.entities.Permission;
 import com.grapplermodule1.GrapplerEnhancement.entities.TeamMembers;
 import com.grapplermodule1.GrapplerEnhancement.entities.Users;
+import com.grapplermodule1.GrapplerEnhancement.service.HierarchyService;
+import com.grapplermodule1.GrapplerEnhancement.service.TeamMembersService;
+import com.grapplermodule1.GrapplerEnhancement.service.TeamService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/team-members")
 public class TeamMemberController {
 
+    private static final Logger log = LoggerFactory.getLogger(HierarchyController.class);
+
+    @Autowired
+    private HierarchyService hierarchyService;
+
+    @Autowired
+    private TeamMembersService teamMembersService;
+
+    /**
+     * For Getting Team Members By Team ID
+     *
+     * @return ResponseEntity<?>
+     */
     @GetMapping("/{teamId}")
-    public ResponseEntity<List<Users>> getTeamMembers(@PathVariable("teamId") Long teamId){
-        return null;
+    public ResponseEntity<?> getTeamMembers(@PathVariable("teamId") Long teamId){
+        String debugUuid = UUID.randomUUID().toString();
+        try {
+            log.info("Get Team Hierarchy By Id API Called, UUID {}", debugUuid);
+            List<TeamMembersDTO> optionalListOfUsers = hierarchyService.getTeamHierarchyById(teamId);
+
+            return new ResponseEntity<>(optionalListOfUsers, HttpStatus.OK);
+        }
+        catch (TeamMembersNotFoundException e) {
+            log.error("UUID {} TeamMembersNotFoundException In Get Team By Id API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (TeamNotFoundException e) {
+            log.error("UUID {} TeamNotFoundException In Get Team By Id API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e) {
+            log.error("UUID {} Exception In Get Team By Id API Exception {} ", debugUuid, e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping("/{teamId}/member/{memberId}")
-    public ResponseEntity<Users> getTeamMembers(@PathVariable("teamId") Long teamId, @PathVariable("memberId") Long memberId){
-        return null;
+    /**
+     * For Getting Team Member Details
+     * 
+     * @return ResponseEntity<?>
+     */
+    @GetMapping("/{teamId}/member/{userId}")
+    public ResponseEntity<?> getTeamMemberDetails(@PathVariable("teamId") Long teamId, @PathVariable("userId") Long userId){
+        String debugUuid = UUID.randomUUID().toString();
+        try {
+            log.info("Get Team Member Details API Called, UUID {}", debugUuid);
+            UsersDTO userDTO = teamMembersService.getMemberDetail(teamId, userId);
+
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
+        }
+        catch (MemberNotPresentInTeamException e) {
+            log.error("UUID {} MemberNotPresentInTeamException In Add New Member API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (UserNotFoundException e) {
+            log.error("UUID {} UserNotFoundException In Add New Member API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (TeamNotFoundException e) {
+            log.error("UUID {} TeamNotFoundException In Add New Member API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e) {
+            log.error("UUID {} Exception In Add New Member API, Exception {} ", debugUuid, e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    /**
+     * For Adding New Member In Team With Team ID and User ID
+     *
+     * @return ResponseEntity<?>
+     */
     @PostMapping("/{teamId}/add-new-member/{userId}")
-    public ResponseEntity addNewMember(@PathVariable("userId") Long userId,
-                                       @PathVariable("teamId") Long teamId){
-        return null;
+    public ResponseEntity<?> addNewMember(@PathVariable("userId") Long userId,
+                                            @PathVariable("teamId") Long teamId){
+        String debugUuid = UUID.randomUUID().toString();
+        try {
+            log.info("Add New Member API Called, UUID {}", debugUuid);
+            TeamMembers teamMember = teamMembersService.addNewMember(teamId, userId);
+
+            return new ResponseEntity<>(new CustomResponseMessage(true, "New Member Add Successfully."), HttpStatus.OK);
+        }
+        catch (UserNotFoundException e) {
+            log.error("UUID {} UserNotFoundException In Add New Member API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (TeamNotFoundException e) {
+            log.error("UUID {} TeamNotFoundException In Add New Member API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponse<>(false, e.getMessage(), null), HttpStatus.NOT_FOUND);
+        }
+        catch (MemberAlreadyPresentException e) {
+            log.error("UUID {} MemberAlreadyPresentException In Add New Member API, Exception {}", debugUuid, e.getMessage());
+            return new ResponseEntity<>(new CustomResponseMessage(false, e.getMessage()), HttpStatus.CONFLICT);
+        }
+        catch (Exception e) {
+            log.error("UUID {} Exception In Add New Member API, Exception {} ", debugUuid, e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @DeleteMapping("/{teamId}/delete-member/{memberId}")
+    @DeleteMapping("/{teamId}/delete-member/{userId}")
     public ResponseEntity deleteMember(@PathVariable("teamId") Long teamId,
                                        @PathVariable("memberId") Long memberId){
         return null;
---
+++ src/main/java/com/grapplermodule1/GrapplerEnhancement/service/TeamMembersService.java
@@ -0,0 +1,115 @@
+package com.grapplermodule1.GrapplerEnhancement.service;
+
+import com.grapplermodule1.GrapplerEnhancement.customexception.MemberAlreadyPresentException;
+import com.grapplermodule1.GrapplerEnhancement.customexception.MemberNotPresentInTeamException;
+import com.grapplermodule1.GrapplerEnhancement.customexception.TeamNotFoundException;
+import com.grapplermodule1.GrapplerEnhancement.customexception.UserNotFoundException;
+import com.grapplermodule1.GrapplerEnhancement.dtos.TeamDTO;
+import com.grapplermodule1.GrapplerEnhancement.dtos.TeamMembersDTO;
+import com.grapplermodule1.GrapplerEnhancement.dtos.UsersDTO;
+import com.grapplermodule1.GrapplerEnhancement.entities.Team;
+import com.grapplermodule1.GrapplerEnhancement.entities.TeamMembers;
+import com.grapplermodule1.GrapplerEnhancement.entities.Users;
+import com.grapplermodule1.GrapplerEnhancement.repository.TeamMemberRepository;
+import com.grapplermodule1.GrapplerEnhancement.repository.TeamRepository;
+import com.grapplermodule1.GrapplerEnhancement.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeamMembersService {
+    private static final Logger log = LoggerFactory.getLogger(HierarchyService.class);
+
+    @Autowired
+    private TeamMemberRepository teamMemberRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * For Adding New Team Member
+     *
+     * @return TeamMembers
+     */
+    public TeamMembers addNewMember(Long teamId, Long userId) {
+        try {
+            log.info("Add Team Member Service Called");
+            Optional<TeamMembers> optionalTeamMember = teamMemberRepository.findByTeamIdAndUserId(teamId, userId);
+
+            if(!optionalTeamMember.isPresent()) {
+                Optional<Team> optionalTeam = teamRepository.findById(teamId);
+
+                if (optionalTeam.isPresent()) {
+                    Optional<Users> optionalUser = userRepository.findById(userId);
+                    if (optionalUser.isPresent()) {
+                        TeamMembers teamMembers = new TeamMembers(optionalTeam.get(), optionalUser.get());
+
+                        return teamMemberRepository.save(teamMembers);
+                    } else {
+                        log.error("Add Team Member Service throws UserNotFoundException");
+                        throw new UserNotFoundException("User Not Found With ID : " + userId);
+                    }
+                } else {
+                    log.error("Add Team Member Service throws TeamNotFoundException");
+                    throw new TeamNotFoundException("Team Not Found With ID : " + teamId);
+                }
+            }
+            else {
+                log.error("Add Team Member Service throws MemberAlreadyPresentException");
+                throw new MemberAlreadyPresentException("Member Already Present In Same Team.");
+            }
+        }
+        catch (Exception e) {
+            log.error("Exception In Add Team Member Exception {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
+     * For Get Member Details
+     *
+     * @return UserDTO
+     */
+    public UsersDTO getMemberDetail(Long teamId, Long userId) {
+        try {
+            log.info("Get Member Details Service Called, User Id {}", userId);
+            Optional<Team> optionalTeam = teamRepository.findById(teamId);
+
+            if(optionalTeam.isPresent()){
+                Optional<TeamMembers> optionalTeamMember = teamMemberRepository.findByTeamIdAndUserId(teamId, userId);
+
+                if (optionalTeamMember.isPresent()) {
+                    Optional<UsersDTO> optionalUserDTO = userRepository.findUserDtoById(userId);
+
+                    if (optionalUserDTO.isPresent()) {
+                        log.info("Get Member Details Service Returning UserDTO, User Id {}", userId);
+                        return optionalUserDTO.get();
+                    }
+                    else {
+                        log.error("Get Member Details Service throws UserNotFoundException");
+                        throw new UserNotFoundException("User Not Found With ID : " + userId);
+                    }
+                }
+                else {
+                    log.error("Get Member Details Service throws TeamNotFoundException");
+                    throw new MemberNotPresentInTeamException("User With ID : " + userId + " Not Found In Team With ID : " + teamId);
+                }
+            }
+            else {
+                log.error("Get Member Details Service throws TeamNotFoundException");
+                throw new TeamNotFoundException("Team Not Found With ID : " + teamId);
+            }
+        } catch (Exception e) {
+            log.error("Exception in Fetch User By Id Exception {}", e.getMessage());
+            throw e;
+        }
+    }
+}
